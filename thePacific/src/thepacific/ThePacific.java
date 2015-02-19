0c0eb1f1634a700620df84b228533865eb1fb497
Ches <Ches@Ches-PC>
2015-02-19 09:45:22 -0700

    Test Function 1 through 4

--- thePacific/src/thepacific/ThePacific.java
+++ thePacific/src/thepacific/ThePacific.java
@@ -5,15 +5,7 @@
  */
 package thepacific;
 
-import Model.Actor;
-import Model.Game;
-import Model.Inventory;
-import Model.Items;
-import Model.Jeep;
-import Model.Location;
-import Model.Map;
-import Model.Player;
-import Model.Scene;
+import view.StartProgramView;
 
 /**
  *
@@ -25,78 +17,9 @@ public class ThePacific {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Player playerOne = new Player();
-        
-        playerOne.setName("Arnold S");
-        playerOne.setBestTime(7.00);
-        playerOne.setHealth(10);
-        
-        String playerInfo = playerOne.toString();
-        System.out.println(playerInfo);
-        
-// map test 
-        Map mapOne = new Map();
-        mapOne.setRowCount(5);
-        mapOne.setColumnCount(5);
-        
-        String mapInfo = mapOne.toString();
-        System.out.println(mapInfo);
-       
-//Inventory 
-        Inventory InventoryOne = new Inventory();
-        
-        InventoryOne.setItemType("Tool");
-        InventoryOne.setQuantity(5);
-        InventoryOne.setMaxWeight(100);
-        
-        String InventoryInfo = InventoryOne.toString();
-        System.out.println(InventoryInfo);
- //Jeep class test
-        Jeep JeepOne = new Jeep();
-        JeepOne.setMaxWeight(10);
-        JeepOne.setCurrentWeight(5);
-        
-        String JeepInfo = JeepOne.toString();
-        System.out.println(JeepInfo);
- //Actor test
-        Actor ActorOne = new Actor();
-        ActorOne.setName("Bob Marley");
-        ActorOne.setType("RobCop");
-        ActorOne.setCoordinates(5.5);
-        
-        String ActorInfo = ActorOne.toString();
-        System.out.println(ActorInfo);
-  //Items test
-        Items ItemsOne = new Items();
-        ItemsOne.setType("Chain Saw");
-        ItemsOne.setDecription("used to cut trees.");
-        
-        String ItemsInfo= ItemsOne.toString();
-        System.out.println(ItemsInfo);
-  //scene testing
-            Scene SceneOne= new Scene();
-            SceneOne.setType("The Hatch");
-            SceneOne.setDescription("In this secret location, you will find the bomb.");
-            SceneOne.setTravelTime(8.0);
-            SceneOne.setSymbol("$$$");
-            
-            String SceneInfo= SceneOne.toString();
-            System.out.println(SceneInfo);
-  //game testing
-        Game GameOne= new Game();
-        GameOne.setTime(8.00);
-        GameOne.setWinner("Travis Wheelwright");
-        
-        String GameInfo= GameOne.toString();
-        System.out.println(GameInfo);
-  //Location Testing
-        Location LocationOne= new Location();
-        LocationOne.setColumn(1);
-        LocationOne.setRow(5);
-        LocationOne.setVisited(true);
-                
-        String LocationInfo= LocationOne.toString();
-        System.out.println(LocationInfo);
+        // create and test the startProgram 
+        StartProgramView StartProgramView = new StartProgramView();
+        StartProgramView.startProgram();
     }
     
 }
---
+++ thePacific/src/view/StartProgramView.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ches
+ */
+public class StartProgramView {
+   public StartProgramView(){
+   }
+    public void startProgram(){
+    //Display the banner screen
+        this.displayBanner();
+    //Get the players name
+        String playersName= this.getPlayersName();
+    //Create a new player
+    //DISPLAY a customized welcome message
+    //  DISPLAY the main menu
+    //END 
+    
+}
+
+    private void displayBanner() {
+        System.out.println("\n\n**********************************************");
+    }
+
+    private String getPlayersName() {
+        boolean valid= false; // indicates if the name is retrieved
+        String playersName = null;
+        Scanner keyboard = new Scanner(System.in);//keyboard input stream
+        
+        while(!valid) { // while valid is not yet retrieved.
+            //prompt for the players name
+            System.out.println("Enter Your Name");
+            
+            //get the name from the keyboard
+            playersName = keyboard.nextLine();
+            playersName = playersName.trim();
+            
+            //if the name is valid
+            if (playersName.length() < 2){
+                System.out.println("Name length must be longer then 2 characters");
+                continue; // repeat again
+            }
+            
+            break; // get out of the repetition 
+            
+            }
+        return playersName; //return the players name
+        }
+        
+    }
+
